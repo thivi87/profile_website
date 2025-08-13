5087b1f12a10be4732ed749b0a654daba6d65c0f
thivi87 <sthivierge@gmail.com>
2025-08-13 12:23:22 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/security-portfolio/" // ← CHANGE to your repo name
+  base: "/Profile_website/" 
 });
