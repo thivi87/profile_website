9961dc8cb6bc352b0e53737bc0c7dfb7fe41e971
thivi87 <sthivierge@gmail.com>
2025-08-15 12:32:42 -0600

    Update vite.config.ts
    Signed-off-by: thivi87 <sthivierge@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/profile_website/", // <- EXACTLY your repo name with leading/trailing slashes
+  base: "/profile_website/",   // MUST match your repo name
 });
