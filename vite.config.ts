cd6fa9b3157ec0a7010227f99a5d1d56deb71d38
thivi87 <sthivierge@gmail.com>
2025-08-19 09:45:01 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/profile_website/", // <— MUST match the repo name
+  base: "/profile_website/",
 });
